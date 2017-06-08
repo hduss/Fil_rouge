fff4df516f2efc7b0ceb3386b1fd6f279984f2fe
hduss <delmas.theo@laposte.net>
2017-06-08 14:53:23 +0200

    isValid OK git status! need to verify and see if we need finla method

--- scr/controllers/RegistrationCtrl.js
+++ scr/controllers/RegistrationCtrl.js
@@ -12,13 +12,62 @@ class RegistrationCtrl {
 
 	post(req, res) {
 
+		let VALID = false;
+
+		// instance of isValid
 		const IsValid = new isValid();
 
-		IsValid.validPseudo(req.body.pseudo);
-		IsValid.validPassword(req.body.pass);
-		IsValid.validComparePassword(req.body.pass, req.body.passconfirm);
+		let validLastName = IsValidLastName(req.body.lastname);
+		let validFirstName = IsValid.validFirstName(req.body.firstname);
+		let validMail = IsValid.validMail(req.body.mail);
+		let validPseudo = IsValid.validPseudo(req.body.pseudo);
+		let validPass = IsValid.validPassword(req.body.pass);
+		let validPassCompare = IsValid.validComparePassword(req.body.pass, req.body.passconfirm);
+		let validAge = IsValidAge(req.body.age);
 
 		console.log('POST OK');
+
+		// all verifications for the registration form 
+		if (validMail) {
+			VALID = true;
+
+			if (validPseudo) {
+				VALID = true;
+
+				if (validPass && validPassCompare) {
+
+					VALID = true;
+					if (validFirstName && validLastName && validAge) {
+
+						VALID = true;
+					}else{
+
+						VALID = false;
+					}
+				}else{
+
+					VALID = false;
+				}
+			}else{
+
+				VALID = false;
+			};
+
+		}else{
+
+			VALID = false;
+		}
+
+		// if everything is valid / true
+		if (VALID) {
+
+			console.log('EVERYTHINGGUCCI !!');
+			// send data in Database
+			// envoi en BDD
+		}else{
+
+			res.redirect('/registration');
+		}
 		
 		
 
--- scr/services/isValid.js
+++ scr/services/isValid.js
@@ -1,3 +1,5 @@
+
+
 class isValid {
 
 	validMail(userMail) {
@@ -9,17 +11,20 @@ class isValid {
 		// compare userMail with regex
 		const compareMail = userMail.match(re);
 
-
 		if (compareMail) {
 
 			console.log('COMPARED OK !!! ');
+			return true;
 
 		}else{
 
 			console.log('COMPARED FAILED !!! ');
+			return false;
 		}
 
 		console.log(compareMail);
+
+		// verifier si le mail est deja present dans la base de données 
 
 
 	}
@@ -30,51 +35,98 @@ class isValid {
 		if (userPseudo && userPseudo.length > 8) {
 
 			console.log('PSEUDO VALID !');
+			return true;
+
+		}else{
+
+			return false;
 		};
+
+
+		// verifier si le pseudo est deja existant
 	}
 
 
 	validPassword(userPassword) {
 
-		console.log(userPassword);
-
-		const caracteres = ["#", "!", "§","" ];
-
-		for (var i = 0; i < userPassword.length; i++) {
-
-			console.log(userPassword[i]);
-			return userPassword[i];
+		
+		// string of special caracteres for valid password
+		const caracteres = "$£!§:/;.,?";
 
 
-			for (var i = 0; i < caracteres.length; i++) {
+		for (var i = 0; i < caracteres.length; i++) {
 
-				console.log(caracteres[i]);
-				return caracteres[i];
+			// indexOf return -1 if false and return integer if true :)
+			let verif = userPassword.indexOf(caracteres[i]);
+			
+			console.log(caracteres[i]);
 
-				};
+			if (verif > 0  && verif.length > 8 ) {
+
+				console.log('PASSWORD VALID');
+				return true;
+
+			}else{
+
+				console.log('PASSWORD INVALID !!! ')
+				return false;
 			};
 
 
-			if (userPassword.length > 8 && userPassword[i] === caracteres[i]) {
-
-				console.log('PASSWORD OK !!! ');
-			};
 		}
+}
 
 
 	
 
 	validComparePassword(userPassword, verifPassword) {
 
-		if (userPassword === verifPassword) {
+		if (userPassword && verifPassword && userPassword === verifPassword) {
 
 			console.log('VERIF PASSWORD OK !!!');
+			return true;
 		};
 	}
 
 
-	
+	validFirstName(firstName) {
+
+		if (firstName && typeof firstName ==="string") {
+
+			console.log("FIRSTNAME VALID !!");
+			return true;
+		}else{
+
+			console.log('FIRSTNAME INVALID');
+			return false;
+		};
+	}
+
+	validLastName(lastName) {
+
+		if (lastName && typeof lastName ==="string") {
+			
+			console.log("LASTNAME VALID !!");
+			return true;
+		}else{
+
+			console.log('LASTNAME INVALID');
+			return false;
+		};
+	}
+
+	validAge(age) {
+
+		if (age >= 18) {
+			console.log('AGE VALID');
+			return true;
+		}else{
+
+			return false;
+		};
+	}
 }
+
 
 module.exports = isValid;
 
